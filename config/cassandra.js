72d79eb267afe14ae75c43cdba466c9af50bb48d
Salomao Vilaca <salomaovilacas@gmail.com>
2018-10-23 08:24:01 -0400

    AAA

--- config/cassandra.js
+++ config/cassandra.js
@@ -2,3 +2,13 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({'contactPoints': ['localhost'], 'keyspace': 'test'});
 
 module.exports = client;
+
+// const cassandra = require('cassandra-driver');
+// const databaseConstant = require('../resource/constant/database');
+
+// const client = new cassandra.Client({'contactPoints': [databaseConstant.IP_DB], 'keyspace': databaseConstant.DB});
+
+// module.exports = function() {
+
+//     return client;
+// };
